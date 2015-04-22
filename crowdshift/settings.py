909517c580fdbb85d262fe875baf6bca9c43aad7
kennykhoapham <kennykhoapham@gmail.com>
2015-04-22 00:57:15 -0700

    postgres

--- crowdshift/settings.py
+++ crowdshift/settings.py
@@ -63,6 +63,12 @@ DATABASES = {
     'default': {
         'ENGINE': 'django_postgrespool',
         #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+            'NAME': 'd2one8uovq33oi',                      # Or path to database file if using sqlite3.
+            # The following settings are not used with sqlite3:
+            'USER': 'aplvndlnovsejk',
+            'PASSWORD': 'TO7FLfsfGu4Q7HAev4gkrauflt',
+            'HOST': 'ec2-50-17-192-136.compute-1.amazonaws.com',                      # Empty for localhost through domain sockets or           '127.0.0.1' for localhost through TCP.
+            'PORT': '5432',                      # Set to empty string for default.
     }
 }
 
