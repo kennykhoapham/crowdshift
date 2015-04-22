a0f172be4d7e9931ee6cc49580971ac1e816d321
kennykhoapham <kennykhoapham@gmail.com>
2015-04-22 01:43:45 -0700

    landing page urls setting

--- crowdshift/urls.py
+++ crowdshift/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'crowdshift.views.home', name='home'),
+    url(r'^$', 'crowdshift.views.index', name='index'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^reviews/', include('reviews.urls')),
     url(r'^admin/', include(admin.site.urls)),
